b855ca384d678375d34a5769f012d9dac46f05dc
Noah Cowan <nsncowan@gmail.com>
2023-02-15 20:55:56 -0800

    save

--- ShippingCompany/Controllers/ParcelsController.cs
+++ ShippingCompany/Controllers/ParcelsController.cs
@@ -9,9 +9,11 @@ namespace ShippingCompany.Controllers
     [HttpGet("/parcels")]
     public ActionResult Index()
     {
+
       return View();
       // is a list neccessary because the data won't be stored anywhere without it???
       // alternatively, can we transfer inputted form data without storing an instance of parcel?
+      // YES YOU NEED A LIST! IT FUNCTIONS AS A ERSATZ DATABASE, THAT IS HOW THE INSTANCES ARE STORED AND TRANSPORTED FROM FUNCTION TO FUNCTION.
     }
 
     [HttpGet("/parcels/new")]
